e4566bd7fa168eeae910af139d54d9bfcaad4d1a
wooooozin <wooooozin@gmail.com>
2023-07-26 23:47:35 +0900

    날씨 일기 조회 기능 구현 (READ)

--- src/main/java/zerobase/weather/service/DiaryService.java
+++ src/main/java/zerobase/weather/service/DiaryService.java
@@ -15,6 +15,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.time.LocalDate;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -41,6 +42,14 @@ public class DiaryService {
         nowDiary.setText(text);
         nowDiary.setDate(date);
         diaryRepository.save(nowDiary);
+    }
+
+    public List<Diary> readDiary(LocalDate date) {
+        return diaryRepository.findAllByDate(date);
+    }
+
+    public List<Diary> readDiaries(LocalDate startDate, LocalDate endDate) {
+        return diaryRepository.findAllByDateBetween(startDate, endDate);
     }
 
     private String getWeatherString() {
@@ -91,4 +100,5 @@ public class DiaryService {
         resultMap.put("temp", mainData.get("temp"));
         return resultMap;
     }
+
 }
