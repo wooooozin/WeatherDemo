471c1225e3a6ac3ad76c96572f491915a84b265e
wooooozin <wooooozin@gmail.com>
2023-08-04 06:49:07 +0900

    Exception class rename

--- src/main/java/zerobase/weather/error/InvalidDateException.java
+++ src/main/java/zerobase/weather/error/InvalidDateException.java
@@ -1,9 +1,9 @@
 package zerobase.weather.error;
 
-public class InvalidDate extends RuntimeException {
+public class InvalidDateException extends RuntimeException {
     private static final String MESSAGE = "오랜 과거 혹은 너무 먼 미래의 날짜 입니다.";
 
-    public InvalidDate() {
+    public InvalidDateException() {
         super(MESSAGE);
     }
 }
